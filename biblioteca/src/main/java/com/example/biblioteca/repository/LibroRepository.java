4e1ae621f820ab09d6a32705941402fcef0ecc2d
PABLO ALVAREZ FERNANDEZ <117449906+EldandydeBarcelona@users.noreply.github.com>
2025-10-27 20:31:44 +0100

    operaciones cruz

---
+++ biblioteca/src/main/java/com/example/biblioteca/repository/LibroRepository.java
@@ -0,0 +1,12 @@
+package com.example.biblioteca.repository;
+
+import com.example.biblioteca.entity.Libro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface LibroRepository extends JpaRepository<Libro,Long> {
+
+}
